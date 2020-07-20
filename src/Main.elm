bf9ff7e31936b45fccf31e25e271fd9ee8feab63
koshy <ben.chenathara@gmail.com>
2020-07-20 23:02:56 +1000

    Fix: enlargen transparent area around coloured square box

--- src/Main.elm
+++ src/Main.elm
@@ -38,34 +38,47 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
+
+        
+    div []
+        [    svg
+            [ width "120"
+            , height "1000"
+            , viewBox "0 0 120 1000"
+            ]
+            (encompassedRectangle model)
+            
+        ]
+
+encompassedRectangle : Model -> List (Svg Msg)
+encompassedRectangle model =
     let
         color = if model.zapped == True then
                   "red"
                 else 
-                   "black"         
-    in
-        
-    div []
-        [    svg
-    [ width "120"
-    , height "1000"
-    , viewBox "0 0 120 1000"
-    ]
-    [ rect
-        [ x "10"
-        , y "10"
-        , width "100"
-        , height "100"
-        , rx "15"
-        , ry "15"
-        , onClick Zap
-        , fill color
+                   "black"     
+        animationFactor = animate [ from "0", to "1000", begin "0s", dur "3s", repeatCount "indefinite", attributeName "y"] []    
+    in    
+        [   rect
+                [ width "120"
+                , height "120"
+                , onClick Zap
+                , fill color
+                , opacity "0"
+                ] [animationFactor] 
+        ,
+            rect
+                [ width "100"
+                , height "100"
+                , onClick Zap
+                , fill color
+                , x "10", y "10"
+                , rx "15", ry "15"
+                ]
+                [   animationFactor
+                ]
         ]
-        [animate [ from "0", to "1000", begin "0s", dur "3s", repeatCount "indefinite", attributeName "y"] []]
-    ]
-        ]
-
-
+            
 
 ---- PROGRAM ----
 
