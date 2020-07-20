93d87fd2699143ac75206afd00a650ac5567ab90
BK <benkoshy@hotmail.com>
2020-07-20 19:08:03 +1000

    Updates

---
+++ precache-manifest.af00c448d6a87b10cb1bb570b85947a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/box-game/favicon.ico"
+  },
+  {
+    "revision": "37d554ded2301f920e27cf337b326ee5",
+    "url": "/box-game/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/box-game/logo.svg"
+  },
+  {
+    "revision": "efc4fde36f64d90a8f0a7252450c774c",
+    "url": "/box-game/manifest.json"
+  },
+  {
+    "revision": "6310c5f7c61dfc37ba08",
+    "url": "/box-game/static/css/main.7f457dfb.chunk.css"
+  },
+  {
+    "revision": "6310c5f7c61dfc37ba08",
+    "url": "/box-game/static/js/main.6310c5f7.chunk.js"
+  },
+  {
+    "revision": "4c58cecdaede16b82b81",
+    "url": "/box-game/static/js/runtime~main.4c58cecd.js"
+  },
+  {
+    "revision": "b9063112a1dd4b93007d",
+    "url": "/box-game/static/js/vendors~main.b9063112.chunk.js"
+  }
+]);
